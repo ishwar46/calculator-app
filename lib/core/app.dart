cef6a31df4308d89f4b2b437a61f54a31d22e375
Ishwar Chaudhary <ishwarura@gmail.com>
2023-11-30 23:08:25 +0545

    DONE

--- lib/core/app.dart
+++ lib/core/app.dart
@@ -1,11 +1,22 @@
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:calculatorapp/views/clculator_page.dart';
+import 'package:flutter/material.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Calculator App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const CalculatorApp(),
+    );
   }
+}
+
+class MyRoutes {
+  static const String calculator = '/calculator';
 }
